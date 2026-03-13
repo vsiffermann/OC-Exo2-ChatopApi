d22b3903568d2514eb72c57e0e9e38efadc98526
vsiffermann <vsiffermann@divalto.com>
2026-03-13 14:47:06 +0100

    Initial Commit

---
+++ src/main/java/com/openclassrooms/chatopapi/ChatopApiApplication.java
@@ -0,0 +1,13 @@
+package com.openclassrooms.chatopapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatopApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatopApiApplication.class, args);
+    }
+
+}
