c932a5281f3d10a0d7dcacbde0bf46fc727fc504
vsiffermann <vsiffermann@divalto.com>
2026-03-18 14:40:46 +0100

    add documentation to user controller

--- src/main/java/com/openclassrooms/chatopapi/controller/UserController.java
+++ src/main/java/com/openclassrooms/chatopapi/controller/UserController.java
@@ -2,6 +2,11 @@ package com.openclassrooms.chatopapi.controller;
 
 import com.openclassrooms.chatopapi.dto.UserDTO;
 import com.openclassrooms.chatopapi.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -9,10 +14,17 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/user")
 @RequiredArgsConstructor
+@Tag(name = "Users", description = "User endpoints")
 public class UserController {
 
     private final UserService userService;
 
+    @Operation(summary = "Get user by ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "User found"),
+            @ApiResponse(responseCode = "401", description = "Unauthorized", content = @Content),
+            @ApiResponse(responseCode = "404", description = "User not found", content = @Content)
+    })
     @GetMapping("/{id}")
     public ResponseEntity<UserDTO> getUserById(@PathVariable Integer id) {
         return ResponseEntity.ok(userService.getUserById(id));
